8e237f5e5cc4b738834b8be8397960277006c091
KaranMishra <93134411+Karan-Kumar-Mishra@users.noreply.github.com>
2025-01-12 17:31:14 +0530

    Delete frontend directory

--- frontend/src/App.js
+++
@@ -1,9 +0,0 @@
-import './App.css';
-import Root from './Components/root/Root';
-function App() {
-  return (
-   <Root/>
-  );
-}
-
-export default App;
--- frontend/src/Components/Redux-thunk/Reducers/index.js
+++
@@ -1,9 +0,0 @@
-// src/redux/reducers/index.js
-import { combineReducers } from 'redux';
-import dataReducer from './DataReducers';
-
-const rootReducer = combineReducers({
-  Data: dataReducer,
-}); 
-
-export default rootReducer;
